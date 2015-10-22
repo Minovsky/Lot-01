d91509cd32c61c869ae37a408a8e6b6e7e15ecab
Michael Brown <gamer5108@hotmail.com>
2015-10-22 01:21:58 -0600

    Add car, simple npc car, and simple spawner of cars

---
+++ Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSpawner : MonoBehaviour
+{
+    public GameObject npcCarPrefab;
+    public GameObject playerCarPrefab;
+
+    public uint numberOfNPCs = 8;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Car newCar = ((GameObject)Instantiate(npcCarPrefab, Vector2.zero, Quaternion.identity)).GetComponent<Car>();
+
+        World.WorldCoord teleLocation = new World.WorldCoord(0, 0);
+        World.WorldCoord teleDir = new World.WorldCoord(1, 0);
+        newCar.TeleportTo(teleLocation, teleDir);
+    }
+}
---
+++ Assets/Scripts/NPCCar.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPCCar : Car
+{
+
+    // Use this for initialization
+    public override void Start ()
+    {
+
+    }
+
+    public void MoveRandomDirection()
+    {
+        World.WorldCoord[] possibleDirections =
+        {
+            direction,
+            new World.WorldCoord(direction.y, direction.x),
+            new World.WorldCoord(-direction.y, -direction.x),
+        };
+
+        List<World.WorldCoord> directions = new List<World.WorldCoord>();
+        foreach(var dir in possibleDirections)
+        {
+            if(World.Instance.CanMoveInto(worldLocation + dir, dir) && !World.Instance.IsParkingSpot(worldLocation+dir))
+                directions.Add(dir);
+        }
+
+        if(directions.Count > 0)
+        {
+            World.WorldCoord chosenDirection = directions[Random.Range(0, directions.Count)];
+
+            MoveIfPossible(chosenDirection);
+        }
+    }
+
+    protected override void OnDestinationReached()
+    {
+        MoveRandomDirection();
+    }
+}
