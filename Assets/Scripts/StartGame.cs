8431d8166f8d4c3d2a22f1c3b291dd525ff5bb46
Krishna Teja Salimeda <krishnasalimeda@gmail.com>
2015-10-27 21:00:27 -0600

    Added Menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public Sprite start;
+	public Sprite startActive;
+	public GameObject menu;
+
+	private SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		menu = GameObject.FindGameObjectWithTag ("Menu");
+	}
+
+	void OnMouseEnter()
+	{
+		spriteRenderer.sprite = startActive;
+	}
+
+	void OnMouseExit()
+	{
+		spriteRenderer.sprite = start;
+	}
+
+	void OnMouseDown()
+	{
+		menu.SetActive (false);
+	}
+}
